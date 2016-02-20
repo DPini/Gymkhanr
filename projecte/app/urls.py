707147e464d034201d6e263b5f03b405437083d0
David Pinilla Caparrós <dpinitux@gmail.com>
2016-02-21 00:58:48 +0100

    Cambios sin acabar de forms.

--- projecte/app/urls.py
+++ projecte/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^Gymcana/(?P<pk>[0-9]+)/$', views.proves_gymcana),
 	url(r'^Gymcana/(?P<pk>[0-9]+)/(?P<idprova>[0-9]+)/$', views.detalls_prova),
+	url(r'^Gymcana/realitza_prova/(?P<idprova>[0-9]+)/$'. views.)
     ]
--- projecte/app/views.py
+++ projecte/app/views.py
@@ -4,7 +4,7 @@ from .models import Gymcana
 # Create your views here.
 
 def proves_gymcana(request,pk):
-    
+
     proves = Prova.objects.filter(numgym=pk)
     gymcana = Gymcana.objects.get(pk=pk)
     return render(request, 'app/proves_gymcana.html', {'proves': proves, 'gymcana': gymcana})
@@ -12,3 +12,14 @@ def detalls_prova(request, idprova, pk):
     prova = Prova.objects.get(pk = idprova)
     gymcana = Gymcana.objects.get(pk=pk)
     return render(request, 'app/detalls_prova.html', {'prova': prova, 'gymcana': gymcana})
+
+from .forms import ProvaCodiForm
+
+def realitza_prova_codi(request):
+    if request.method == 'POST':
+        form = ProvaCodiForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("http://google.com")
+        else
+            form = ProvaCodiForm();
+        return render(request,'app/realitza_prova_codi.html', {'form': form})
